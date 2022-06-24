27881972f1f3b965736a959a637fc375f8ce9736
lukaknezic <luka.knezic@infinum.com>
2022-06-24 13:36:46 +0200

    Export models

--- lib/flutter_locker.dart
+++ lib/flutter_locker.dart
@@ -5,5 +5,7 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 import 'package:flutter_locker/gen/locker_api.gen.dart';
 
+export 'package:flutter_locker/gen/locker_api.gen.dart';
+
 part 'src/flutter_locker.dart';
 part 'src/locker_exception.dart';
